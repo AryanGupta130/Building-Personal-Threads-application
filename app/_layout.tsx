efe6aae431d8f337d4c14c2b2a18b441225a6fde
Aryan Gupta <ag@Aryans-MacBook-Air.local>
2025-09-13 13:18:03 -0400

    Created a good setup to get started with development. ROutes have been defined as well as that cache

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,49 @@
-import { Slot, Stack } from "expo-router";
+import { Slot } from "expo-router";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { tokenCache } from "../utils/cache"
+import { SplashScreen } from "expo-router";
+
+import {
+  useFonts,
+  DMSans_400Regular,
+  DMSans_500Medium,
+  DMSans_700Bold,
+} from '@expo-google-fonts/dm-sans';
+import { useEffect } from "react";
 
 const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
-
-const InitialLayout = () => {
-  return (
-    <Slot />
+if (!clerkPublishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Key is not set.'
   )
 }
 
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSans_500Medium,
+    DMSans_700Bold,
+  });
+
+  useEffect (() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+    }, [fontsLoaded]);
+  return <Slot />;
+};
+
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={clerkPublishableKey!}>
-      <Stack>
-        <Stack.Screen name="index" />
-      </Stack>
+    <ClerkProvider 
+    publishableKey={clerkPublishableKey!}
+    tokenCache={tokenCache}
+    >
+      <ClerkLoaded> 
+        <InitialLayout />
+      </ClerkLoaded>
     </ClerkProvider>
   )
 }
